de8a6740faf3212c986fd50d7c3904e578ceff02
Amansaini22 <60693662+Amansaini22@users.noreply.github.com>
2020-05-30 01:43:53 +0530

    Aman saini

---
+++ app/src/main/java/com/example/shopeasy/food_carts.java
@@ -0,0 +1,14 @@
+package com.example.shopeasy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class food_carts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_carts);
+    }
+}
